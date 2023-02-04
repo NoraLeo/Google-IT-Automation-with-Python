be8b70890ca2fd59e28aa218cda7f19ee997d787
NoraLeo <38717394+NoraLeo@users.noreply.github.com>
2023-02-04 10:18:32 +0800

    Remove redundant code
    Removed line for importing colors from reportlab

--- reports.py
+++ reports.py
@@ -2,7 +2,7 @@
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
+
 
 
 def generate_report(filename, title, paragraph):
